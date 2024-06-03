af5658524e45fb18c76d1a4651143c77587b5a20
Mominul <islammominul3378@gmail.com>
2024-06-04 04:14:10 +0600

    added jwt token and cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -7,8 +9,12 @@ const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -32,6 +38,20 @@ async function run() {
     const hotelCollection = client.db('hotelBook').collection('room');
     const bookingCollection = client.db('hotelBook').collection('bookings');
 
+    // auth related api jwt 
+    app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false, 
+      })
+      .send({success: true});
+    })
+
+    // room related api 
     app.get('/room', async(req, res) => {
         const cursor = hotelCollection.find();
         const result = await cursor.toArray();
@@ -50,7 +70,7 @@ async function run() {
         res.send(result);
     })
 
-    // booking
+    // booking api
     app.post('/booking', async(req, res) => {
         const booking = req.body;
         console.log(booking);
